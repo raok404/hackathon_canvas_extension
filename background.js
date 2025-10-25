a1c5dea4af81650688901ab8fee508036e2c2dff
Derek Rosales <derek500sp@gmail.com>
2025-10-25 13:12:39 -0400

    sidebar scrapped

--- background.js
+++ background.js
@@ -5,4 +5,14 @@
 // });
 const welcomePage = 'sidebar.html';
 const mainPage = 'sidebar.html';
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.sidePanel.setOptions({ path: welcomePage });
+    chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+});
 
+chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+    const { path } = await chrome.sidePanel.getOptions({ tabId });
+    if (path === welcomePage) {
+        chrome.sidePanel.setOptions({ path: mainPage });
+    }
+});
