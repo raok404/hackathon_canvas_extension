53a6890654a372ee0a9f18ddb7fcf955da62e4df
Derek Rosales <derek500sp@gmail.com>
2025-10-25 12:20:56 -0400

    made sidebar

--- background.js
+++ background.js
@@ -1,5 +1,19 @@
-chrome.runtime.onInstalled.addListener( () => {
-    chrome.action.setBadgeText({
-        text: "OFF",
-    });
+// chrome.runtime.onInstalled.addListener( () => {
+//     chrome.action.setBadgeText({
+//         text: "OFF",
+//     });
+// });
+const welcomePage = 'sidebar.html';
+const mainPage = 'sidebar.html';
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.sidePanel.setOptions({ path: welcomePage });
+    chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+});
+
+chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+    const { path } = await chrome.sidePanel.getOptions({ tabId });
+    if (path === welcomePage) {
+        chrome.sidePanel.setOptions({ path: mainPage });
+    }
 });
