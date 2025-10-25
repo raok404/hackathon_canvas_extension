5fa402e5c2d5f2760a01149240b19f4ae9416915
Kaydence Rao <dencer797@gmail.com>
2025-10-25 12:42:17 -0400

    update manifest.json and content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,21 @@
+function findElement() {
+  const element = document.querySelector("#right-side");
+  if (element) {
+    console.log("Found immediately:", element);
+    element.style.display = "none"
+    return true;
+  }
+  return false;
+}
+
+
 const observer = new MutationObserver(() => {
   const element = document.querySelector("#right-side");
   if (element) {
-    console.log("Found");
+    console.log("Found with mutation");
     element.style.display = "none"
     observer.disconnect(); // stop observing once found
   }
 });
 observer.observe(document.body, { childList: true, subtree: true });
+
