c9c728fcc34cf4c0951ebb07cadb8aba1c2c4da7
Kaydence Rao <dencer797@gmail.com>
2025-10-25 13:02:34 -0400

    fix content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,21 +1,7 @@
-function findElement() {
-  const element = document.querySelector("#right-side");
-  if (element) {
-    console.log("Found immediately:", element);
-    element.style.display = "none"
-    return true;
+const style = document.createElement("style");
+style.textContent = `
+  #right-side {
+    visibility: hidden !important;
   }
-  return false;
-}
-
-
-const observer = new MutationObserver(() => {
-  const element = document.querySelector("#right-side");
-  if (element) {
-    console.log("Found with mutation");
-    element.style.display = "none"
-    observer.disconnect(); // stop observing once found
-  }
-});
-observer.observe(document.body, { childList: true, subtree: true });
-
+`;
+document.head.appendChild(style);
